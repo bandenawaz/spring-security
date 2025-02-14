150053b5e825e74a0de6fbb5b2dd87d6ebfcd924
Bandenawaz Bagwan <bandenawaz.bagwan@publicissapient.com>
2025-02-14 10:55:53 +0530

    SPring Security Section1 added

---
+++ spring-security-sec1/src/main/java/com/example/spring_security_sec1/SpringSecuritySec1Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_sec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySec1Application.class, args);
+	}
+
+}
---
+++ spring-security-sec1/src/test/java/com/example/spring_security_sec1/SpringSecuritySec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_sec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
